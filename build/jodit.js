aa6fe1833a159eefb9776c28f49be9f2453a2504
mehdy-karimpour <mehdy.karimpour@signumsoftware.com>
2022-09-19 21:40:28 +0430

    fix support local pattern for isURL

--- build/jodit.js
+++ build/jodit.js
@@ -5677,7 +5677,7 @@ exports.isPromise = isPromise;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isURL = void 0;
 function isURL(str) {
-	const localPattern = new RegExp('^\\/(\\/\\w+(.\\w+)*)*', 'i');
+	const localPattern = new RegExp('^\\/(\\w+(.\\w+)*)', 'i');
     var pattern = new RegExp('^(https?:\\/\\/)' +
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' +
         '((\\d{1,3}\\.){3}\\d{1,3}))' +
