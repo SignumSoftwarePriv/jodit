da6752f4a63254a5997e01586c399ff6337a68ad
mehdy-karimpour <mehdy.karimpour@signumsoftware.com>
2025-06-10 17:51:32 +0330

    fix return type checking

--- src/core/storage/engines/local-storage-provider.ts
+++ src/core/storage/engines/local-storage-provider.ts
@@ -63,7 +63,7 @@ export class LocalStorageProvider<T = StorageValueType> implements IStorage<T> {
 
 			const json = buffer ? JSON.parse(buffer) : {};
 
-			return json[key] !== undefined ? json[key] : null;
+			return json[key] !== undefined ? json[key] : null!;
 		} catch {}
 	}
 
